8a6301bd5fe7640a9bf782d9e7d50f953226a40b
legozx777 <153138339+legozx777@users.noreply.github.com>
2026-08-22 07:07:39 -0400

    Feat: add show length to url parsing

--- mus.py
+++ mus.py
@@ -39,7 +39,7 @@ def download(subfolder, url="", extra=""):
         args.append(url)
     elif len(url) == 11: # yt video
         args.append("https://www.youtube.com/watch?v=" + url)
-    elif len(url) == 13 or len(url) == 34 or len(url) == 41: # yt (13, 34 - playlist, 41 - album)
+    elif len(url) == 13 or len(url) == 34 or len(url) == 41 or len(url) == 26: # yt (13, 34 - playlist, 41 - album, 26 - show)
         args.append("https://www.youtube.com/playlist?list=" + url)
     elif "twitch.tv/videos" in url:
         args.pop(1)
